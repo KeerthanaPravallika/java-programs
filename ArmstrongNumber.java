53bb02743de509cd460b863c89d6bcbba2b0d0f1
Keerthana Pravallika Samudrala <66155737+KeerthanaPravallika@users.noreply.github.com>
2021-05-25 12:41:32 +0530

    ArmstrongNumber

---
+++ ArmstrongNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String args[])
+    {
+        int num,i,tmp,d,sumOfCubes=0;
+        System.out.print("Enter the number : ");
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        tmp = num;
+        while(num!=0)
+        {
+            d = num%10;
+            sumOfCubes = sumOfCubes + (d*d*d);
+            num = num/10;
+        }
+        if(tmp == sumOfCubes)
+            System.out.println("It is a Armstrong Number");
+        else
+            System.out.println("It is not a Armstrong Number");
+       
+    }
+}
+
+
+
