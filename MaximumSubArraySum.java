c5b3d16e2f11de6eda6ed3855ad2df0f73aab9a3
Keerthana Pravallika Samudrala <66155737+KeerthanaPravallika@users.noreply.github.com>
2021-05-25 13:19:55 +0530

    MaximumSubArraySum

---
+++ MaximumSubArraySum.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String args[])
+    {
+        int num,i,meh = 0,msf = Integer.MIN_VALUE;
+        System.out.print("Enter the number : ");
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        int []arr = new int[num];
+        System.out.println("Enter the elements : ");
+        for(i=0;i<num;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        for(i = 0;i<arr.length;i++)
+        {
+            meh = meh + arr[i];
+            if(meh < arr[i])
+                meh = arr[i];
+            if(meh > msf)
+                msf = meh;
+        }
+        System.out.println(msf);
+       
+    }
+}
