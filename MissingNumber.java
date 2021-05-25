dacb6304625220ae81ee7684f1f8d90060d05380
Keerthana Pravallika Samudrala <66155737+KeerthanaPravallika@users.noreply.github.com>
2021-05-25 12:57:35 +0530

    MissingNumber

---
+++ MissingNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String args[])
+    {
+        int num,i,sumt=0,sumo=0;
+        System.out.print("Enter the number : ");
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        int []arr = new int[num];
+        System.out.println("Enter the elements : ");
+        for(i=0;i<num;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        for(i=0;i<arr.length;i++)
+        {
+            sumt+=arr[i];
+            sumo+=i;
+        }
+        sumo += i;
+        System.out.println(sumo-sumt);
+       
+    }
+}
+
