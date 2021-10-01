3e2b0ed665db1df094e37c8869eff010c175664e
Tamanna Sharma <91528741+Tamanna-S@users.noreply.github.com>
2021-10-01 19:05:50 +0530

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,25 +1,29 @@
-import java.util.*;
-public class PrimeNumber {
-
-	public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	int n = sc.nextInt();
-	 int i,m=0,flag=0;      
-	  m = n/2;      
-	  if(n==0 || n==1){  
-	   System.out.println(n + " is not prime number");      
-	  }
-	  else{  
-	   for(i=2;i<=m;i++){      
-	    if(n % i == 0){      
-	     System.out.println(n+" is not prime number");      
-	     flag=1;      
-	     break;      
-	    }      
-	   }      
-	   if(flag==0)  { 
-		   System.out.println(n+" is prime number"); 
-		   }  
-	  }  
-	}
+import java.util.Scanner;
+public class prime
+{
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        int n;
+        boolean prime = true;
+        System.out.print("Enter a number : ");
+        n = scan.nextInt();
+        for(int i=2 ; i <= n/2 ; i++)
+        {
+            if(n%i == 0)
+            {
+                prime = false;
+                break;
+            }           
+        }
+        if(prime)
+        {
+            System.out.println("it's prime");
+        }
+        else
+        {
+            System.out.println("it's not prime");
+        }
+        scan.close();
+    }
 }
